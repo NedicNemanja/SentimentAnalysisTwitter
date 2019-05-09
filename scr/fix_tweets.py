e3b700252bab5e467911af77d6929988be15aca1
NedicNemanja <sdi1400124@di.uoa.gr>
2019-05-09 21:43:43 +0200

    rename

--- scr/fix_tweets.py
+++ scr/fix_tweets.py
@@ -7,7 +7,7 @@ def remove_chars(tweets,chars):
 #source:https://gist.github.com/gruber/8891611
 def remove_links(tweets):
     import re
-    from src import urlmarker
+    from scr import urlmarker
     for tweet in tweets:
         links=re.findall(urlmarker.URL_REGEX, tweet[1])
         for link in links:
