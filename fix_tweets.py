31bc8157a065885f0c9f1fb4c48f2ee176ba7fe2
Rafael <sdi1400248@di.uoa.gr>
2019-05-09 21:06:14 +0300

    some fixes and link removal

--- fix_tweets.py
+++ fix_tweets.py
@@ -2,4 +2,13 @@
 def remove_chars(tweets,chars):
     for tweet in tweets:
         tweet[1] = ''.join( c for c in tweet[1] if  c not in chars )
-    return tweets
+
+
+#source:https://gist.github.com/gruber/8891611
+def remove_links(tweets):
+    import re
+    import urlmarker
+    for tweet in tweets:
+        links=re.findall(urlmarker.URL_REGEX,tweet[1])
+        for link in links:
+            tweet[1] = tweet[1].replace(link,"")
--- read_files.py
+++ read_files.py
@@ -14,6 +14,6 @@ def read_tsv(file_path):
         csv_input = csv.reader(f_input, delimiter='\t', skipinitialspace=True)
         csv_output = []
         for cols in csv_input:
-            csv_output.append( ( cols[2] ,  cols[3] ) )
+            csv_output.append( [ cols[2] ,  cols[3] ] )
 
     return csv_output
