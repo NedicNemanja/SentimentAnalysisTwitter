db863e8f8bdff3035f413e7ad928ab7dd562a95f
Rafael <sdi1400248@di.uoa.gr>
2019-05-10 15:59:10 +0300

    Word cloud ready added the enchant library

---
+++ scr/colour_wc.py
@@ -0,0 +1,6 @@
+def random_color_func(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
+    h = int(360.0 * 133.0 / 255.0)
+    s = int(100.0 * 255.0 / 255.0)
+    l = int(100.0 * float(random_state.randint(65, 120)) / 255.0)
+
+    return "hsl({}, {}%, {}%)".format(h, s, l)
--- scr/fix_tweets.py
+++ scr/fix_tweets.py
@@ -1,14 +1,34 @@
 
-def remove_chars(tweets,chars):
-    for tweet in tweets:
-        tweet[1] = ''.join( c for c in tweet[1] if  c not in chars )
-
+def remove_chars(tweet,chars):
+        tweet = ''.join( c for c in tweet if  c not in chars )
+        return tweet
 
 #source:https://gist.github.com/gruber/8891611
-def remove_links(tweets):
+def remove_links(tweet):
     import re
-    from scr import urlmarker
+    import urlmarker
+    links=re.findall(urlmarker.URL_REGEX, tweet)
+    for link in links:
+        tweet = tweet.replace(link,"")
+    return tweet
+
+def remove_tags(tweet):
+    tweet = " ".join(filter(lambda x:x[0]!='@', tweet.split()))
+    tweet = " ".join(filter(lambda x:x[0]!='#', tweet.split()))
+    return tweet
+
+
+def wd_rem(tweets):
+    import re
+    import urlmarker
+    symbols="#@,.'\"\\:[]<>1234567890!$%^&*;?/-_+=)("
+    tw_l=[]
     for tweet in tweets:
-        links=re.findall(urlmarker.URL_REGEX, tweet[1])
-        for link in links:
-            tweet[1] = tweet[1].replace(link,"")
+        tw=tweet[1]
+        tw=remove_links(tw)
+        tw = tw.replace("@", " @")
+        tw = tw.replace("#", " #")
+        tw=remove_tags(tw)
+        tw=remove_chars(tw,symbols)
+        tw_l.append(tw)
+    return tw_l
